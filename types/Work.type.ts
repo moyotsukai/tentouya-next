7b860d8f768f10fda7d8fde4214f2aa95d355197
Shinchan-git <gyoshu.bowie@gmail.com>
2021-11-10 23:28:46 +0900

    add: Work.type.ts

---
+++ types/Work.type.ts
@@ -0,0 +1,11 @@
+export type Work = {
+  title: string,
+  descriptionShort: string,
+  descriptionLong: string,
+  imageUrl: string[],
+  shopLink: string,
+  detailLink: string,
+  youTubeLink: string,
+  ogpUrl: string,
+  isPickedUp: boolean
+}
